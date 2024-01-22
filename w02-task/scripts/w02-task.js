dc970ac4e7bc7f73db7af565d7b0a4a0b29cb876
marcelo.carmo <marcelo.carmo54@gmail.com>
2024-01-22 20:09:42 -0300

    update

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,30 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Marcelo dos Santos Carmo';
+
+const currentYear = new Date.parse();
+
+console.log = (currentYear);
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
